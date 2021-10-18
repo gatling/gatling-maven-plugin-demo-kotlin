da37b518bb046113231e0d56c7bbf75445ee8372
Stephane Landelle <slandelle@gatling.io>
2021-10-18 23:05:30 +0200

    Upgrade 3.7.0-M3

--- src/test/kotlin/computerdatabase/BasicSimulation.kt
+++ src/test/kotlin/computerdatabase/BasicSimulation.kt
@@ -1,22 +1,22 @@
 package computerdatabase
 
-import io.gatling.core.javaapi.*
+import io.gatling.javaapi.core.*
 
 import java.time.Duration
 
-import io.gatling.core.javaapi.Predef.*
-import io.gatling.http.javaapi.Predef.*
+import io.gatling.javaapi.core.CoreDsl.*
+import io.gatling.javaapi.http.HttpDsl.*
 
 class BasicSimulation : Simulation() {
 
-  private val httpProtocol = http()
+  val httpProtocol = http
     .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  private val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
+  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .exec(http("request_1")
       .get("/"))
     .pause(7) // Note that Gatling has recorder real time pauses
