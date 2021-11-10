f04af0494e219e229096bbd24b86da8c9f92f487
Stephane Landelle <slandelle@gatling.io>
2021-11-10 17:58:42 +0100

    nit

--- src/test/kotlin/IDEPathHelper.kt
+++ src/test/kotlin/IDEPathHelper.kt
@@ -2,9 +2,10 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 object IDEPathHelper {
-  private val projectRootDir: Path = Paths.get(javaClass.getResource("gatling.conf").toURI()).parent.parent.parent
+  private val projectRootDir = Paths.get(javaClass.getResource("gatling.conf").toURI()).parent.parent.parent
   private val mavenTargetDirectory: Path = projectRootDir.resolve("target")
   private val mavenSrcTestDirectory: Path = projectRootDir.resolve("src").resolve("test")
+
   val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("kotlin")
   val mavenResourcesDirectory: Path = mavenSrcTestDirectory.resolve("resources")
   val mavenBinariesDirectory: Path = mavenTargetDirectory.resolve("test-classes")
