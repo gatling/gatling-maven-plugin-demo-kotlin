2d2aaf460fb2be0721e1ebf43d923d55110f6c35
slandelle <slandelle@gatling.io>
2024-03-18 22:06:34 +0100

    chore: don't rely on gatling.conf

--- src/test/kotlin/IDEPathHelper.kt
+++ src/test/kotlin/IDEPathHelper.kt
@@ -1,15 +1,20 @@
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.Objects.requireNonNull
 
 object IDEPathHelper {
-  private val projectRootDir = Paths.get(requireNonNull(javaClass.getResource("gatling.conf"), "Couldn't locate gatling.conf").toURI()).parent.parent.parent
-  private val mavenTargetDirectory: Path = projectRootDir.resolve("target")
+  val mavenBinariesDirectory: Path =
+          System.getProperty("java.class.path").split(File.pathSeparator)
+                  .map { cpe ->  Path.of(cpe) }
+                  .filter { cpe -> cpe.endsWith("test-classes") }
+                  .findFirst()
+                  .get()
+
+  private val mavenTargetDirectory: Path = mavenBinariesDirectory.parent
+  private val projectRootDir = mavenTargetDirectory.parent
   private val mavenSrcTestDirectory: Path = projectRootDir.resolve("src").resolve("test")
 
   val mavenSourcesDirectory: Path = mavenSrcTestDirectory.resolve("kotlin")
   val mavenResourcesDirectory: Path = mavenSrcTestDirectory.resolve("resources")
-  val mavenBinariesDirectory: Path = mavenTargetDirectory.resolve("test-classes")
   val resultsDirectory: Path = mavenTargetDirectory.resolve("gatling")
   val recorderConfigFile: Path = mavenResourcesDirectory.resolve("recorder.conf")
 }
